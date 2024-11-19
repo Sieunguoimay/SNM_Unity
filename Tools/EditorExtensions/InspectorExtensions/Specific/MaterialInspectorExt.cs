ad013551d3540b1cb668c4f3cca99aa394858ea1
Sieunguoimay <vuduydu1997@gmail.com>
2024-11-19 18:48:28 +0700

    fix namespace

--- Tools/EditorExtensions/InspectorExtensions/Specific/MaterialInspectorExt.cs
+++ Tools/EditorExtensions/InspectorExtensions/Specific/MaterialInspectorExt.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine;
