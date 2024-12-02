2a1c7df3af234b526931087858ef6179188bc464
Sieunguoimay <duvd@phugroup.com>
2024-12-02 17:32:09 +0700

    s

--- Game/Lifecycle/ILifecycle.cs
+++ Game/Lifecycle/ILifecycle.cs
@@ -1,28 +1,10 @@
 namespace SNM.Lifecycle
 {
-    /// <summary>
-    /// This is the interface to the ILifecycleManager
-    /// You want to reference to other objects safely without worrying about
-    /// the order of Destroy() when Unloading the game.
-    /// This Lifecycle system is for you!
-    /// </summary>
     public interface ILifecycle
     {
-        void Initialize(ILifecycleManager manager);//get system ready: spawn internal objects
-        void Dispose();//destroy spawned stuffs
-    }
-
-    public interface IBatchLifecycle : ILifecycle
-    {
-        void AfterInitialize();//connect to sibling systems
-        void BeforeDispose();//disconnect from sibling systems
-    }
-
-    /// <summary>
-    /// Use this interface if you want to destroy the object
-    /// </summary>
-    public interface IAutoDisposeLifecycle
-    {
-        void RequestDispose();
+        void Initialize();    // Handles internal initialization.
+        void Setup();         // Configures with external dependencies.
+        void Teardown();      // Cleans up or disconnects external dependencies.
+        void Cleanup();       // Finalizes and cleans up internal state.
     }
 }
---
+++ Game/Visual/Layout/WorldBounds.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+namespace SNM.Layout
+{
+    public interface IWorldBounds
+    {
+        Bounds Bounds { get; }
+        Transform Transform { get; }
+    }
+
+    public class WorldBounds : MonoBehaviour, IWorldBounds
+    {
+        [SerializeField] private Bounds bounds;
+
+        Bounds IWorldBounds.Bounds => bounds;
+        Transform IWorldBounds.Transform => transform;
+
+        public Vector3 GetRandomPosInside()
+        {
+            var x = Random.Range(bounds.min.x, bounds.max.x);
+            var y = Random.Range(bounds.min.y, bounds.max.y);
+            var z = Random.Range(bounds.min.z, bounds.max.z);
+            return transform.TransformPoint(new Vector3(x, y, z));
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawWireCube(bounds.center, bounds.size);
+        }
+    }
+}
