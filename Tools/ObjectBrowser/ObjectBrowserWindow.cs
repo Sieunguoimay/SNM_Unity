e9abe0032efebcce8869f5d770968e1f481fe270
Sieunguoimay <duvd@phugroup.com>
2025-07-27 16:07:27 +0700

    object browser

---
+++ Tools/ObjectBrowser/ObjectBrowserWindow.cs
@@ -0,0 +1,230 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Sieunguoimay.Tools
+{
+    public class ObjectBrowserWindow : EditorWindow, RuntimeObjectExpose.ITargetObjectProvider
+    {
+        private const string RuntimeObjectBrowser = "Object Browser";
+
+        [MenuItem("Tools/Snm/Object Browser")]
+        public static void OpenWindow()
+        {
+            GetWindow(typeof(ObjectBrowserWindow), false, RuntimeObjectBrowser).Show();
+        }
+
+        public static ObjectBrowserWindow OpenWindowAndReturnSelf()
+        {
+            var window = GetWindow(typeof(ObjectBrowserWindow), false, RuntimeObjectBrowser);
+            window.Show();
+            return window as ObjectBrowserWindow;
+        }
+
+        private Object _rootObject;
+        private string _path;
+        private bool _refreshEveryFrame;
+
+        private IReadOnlyList<RuntimeObjectExpose.ObjectExposedItem> _displayItems;
+        private RuntimeObjectExpose.CommonRuntimeObjectExposeEditor _commonRuntimeObjectExposeEditor;
+
+        private RuntimeObjectExpose _objectExpose;
+        private RuntimeObjectExpose ObjectExpose => _objectExpose ??= new RuntimeObjectExpose(this);
+
+        private RuntimeObjectExpose.CommonRuntimeObjectExposeEditor CommonRuntimeObjectExposeEditor
+        {
+            get { return _commonRuntimeObjectExposeEditor ??= new RuntimeObjectExpose.CommonRuntimeObjectExposeEditor(OnItemClicked); }
+        }
+
+        public object TargetObject { get; private set; }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.BeginHorizontal();
+            GUI.enabled = false;
+            EditorGUILayout.ObjectField(MonoScript.FromScriptableObject(this), GetType(), false);
+            GUI.enabled = true;
+            EditorGUILayout.EndHorizontal();
+
+            GUILayout.Space(10);
+
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.Space(50);
+            var rootObject = EditorGUILayout.ObjectField("Source Object", _rootObject, typeof(Object), true);
+            var canGetComponents = _rootObject is GameObject or Component;
+            if (canGetComponents)
+            {
+                DrawComponentSelectingButton(_rootObject, i =>
+                {
+                    ChangeRootObject(i);
+                    ResetPath();
+                    UpdateCurrentObject();
+                    Expose();
+                });
+            }
+
+            GUILayout.Space(50);
+            EditorGUILayout.EndHorizontal();
+
+            if (_rootObject != rootObject)
+            {
+                ChangeRootObject(rootObject);
+                ResetPath();
+                UpdateCurrentObject();
+                Expose();
+            }
+
+
+            GUILayout.Space(10);
+            EditorGUILayout.BeginHorizontal();
+
+            var enableBackButton = !string.IsNullOrEmpty(_path);
+            var ge = GUI.enabled;
+            GUI.enabled = enableBackButton;
+            if (GUILayout.Button("<-", GUILayout.Width(25)))
+            {
+                RemoveLastPathSegment();
+                UpdateCurrentObject();
+                Expose();
+            }
+
+            GUI.enabled = ge;
+            _path = EditorGUILayout.TextField(_path);
+            if (GUILayout.Button("Browse", GUILayout.Width(60)))
+            {
+                UpdateCurrentObject();
+                Expose();
+            }
+
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            _refreshEveryFrame = GUILayout.Toggle(_refreshEveryFrame, "Refresh every frame");
+            GUILayout.Space(5);
+            EditorGUILayout.EndHorizontal();
+
+            if (_displayItems != null)
+            {
+                CommonRuntimeObjectExposeEditor.DrawExposedItems(_displayItems);
+            }
+
+            if (_rootObject == null)
+            {
+                GUILayout.Box("Drag UnityEngine.Object into the above Object Field", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleCenter, fontSize = 25, wordWrap = true }, GUILayout.ExpandHeight(true), GUILayout.ExpandWidth(true));
+            }
+
+            if (_refreshEveryFrame)
+            {
+                Expose();
+            }
+        }
+
+        private static void DrawComponentSelectingButton(Object rootObject, Action<Object> selectedHandler)
+        {
+            if (!GUILayout.Button("...", GUILayout.Width(20))) return;
+
+            var menu = new GenericMenu();
+            var gameObject = rootObject switch
+            {
+                GameObject go => go,
+                Component co => co.gameObject,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+            IEnumerable<Object> interfaces = gameObject.GetComponents<Component>();
+            interfaces = interfaces.Append(gameObject);
+
+            foreach (var i in interfaces)
+            {
+                menu.AddItem(new GUIContent(i.GetType().Name), rootObject == i, () => { selectedHandler?.Invoke(i); });
+            }
+
+            menu.ShowAsContext();
+        }
+
+        private void OnItemClicked(RuntimeObjectExpose.ObjectExposedItem item)
+        {
+            if (item.MemberInfo is MethodInfo methodInfo)
+            {
+                if (methodInfo.GetParameters().Length == 0)
+                {
+                    methodInfo.Invoke(TargetObject, null);
+                    Debug.Log($"Invoked method {methodInfo.Name} on object {TargetObject}");
+                }
+            }
+            else
+            {
+                GoInto(item);
+            }
+        }
+
+        public void GoInto(RuntimeObjectExpose.ObjectExposedItem item)
+        {
+            GoInto(item.FieldName);
+        }
+
+        private void GoInto(string pathSegment)
+        {
+            AppendPath(pathSegment);
+            UpdateCurrentObject();
+            Expose();
+        }
+
+        public void ChangeRootObject(Object rootObject)
+        {
+            _rootObject = rootObject;
+        }
+
+        public void ResetPath()
+        {
+            _path = "";
+        }
+
+        private void AppendPath(string memberName)
+        {
+            _path = string.Concat(_path, $".{memberName}");
+        }
+
+        private void RemoveLastPathSegment()
+        {
+            var lastIndexOf = _path.LastIndexOf(".", StringComparison.Ordinal);
+            if (lastIndexOf >= 0)
+            {
+                _path = _path[..lastIndexOf];
+            }
+        }
+
+        private void UpdateCurrentObject()
+        {
+            if (!string.IsNullOrEmpty(_path))
+            {
+                var pe = new UnityObjectPathSelector.PathExecutor();
+                pe.Setup(_path, _rootObject, false, false, Application.isPlaying);
+                TargetObject = pe.ExecutePath();
+            }
+            else
+            {
+                TargetObject = _rootObject;
+            }
+
+            if (TargetObject == null || _rootObject == null) return;
+            ObjectExpose.UpdateReflectionInfos();
+        }
+
+        private void Expose()
+        {
+            if (TargetObject == null || _rootObject == null)
+            {
+                _displayItems = null;
+                return;
+            }
+
+            _displayItems = ObjectExpose.ExposeObject();
+        }
+    }
+}
+#endif
---
+++ Tools/ObjectBrowser/RuntimeObjectExpose.cs
@@ -0,0 +1,199 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+
+namespace Sieunguoimay.Tools
+{
+    public class RuntimeObjectExpose
+    {
+        private IEnumerable<FieldInfo> _allFields;
+        private IEnumerable<PropertyInfo> _allProperties;
+        private IEnumerable<MethodInfo> _allMethods;
+        private readonly ITargetObjectProvider _objectProvider;
+
+        public RuntimeObjectExpose(ITargetObjectProvider objectProvider)
+        {
+            _objectProvider = objectProvider;
+        }
+
+        public interface ITargetObjectProvider
+        {
+            object TargetObject { get; }
+        }
+
+        public IReadOnlyList<ObjectExposedItem> ExposeObject()
+        {
+            if (_objectProvider.TargetObject == null) return null;
+            if (_allFields == null || _allProperties == null || _allMethods == null)
+            {
+                UpdateReflectionInfos();
+            }
+
+            var exposedItems = new List<ObjectExposedItem>();
+
+            foreach (var fieldInfo in _allFields)
+            {
+                object value = null;
+                try
+                {
+                    value = fieldInfo.GetValue(_objectProvider.TargetObject);
+                }
+                catch (Exception)
+                {
+                    //ignore
+                }
+
+                exposedItems.Add(new ObjectExposedItem
+                {
+                    FieldName = fieldInfo.Name,
+                    DisplayValue = value?.ToString(),
+                    IsPrimitive = IsPrimitive(fieldInfo.FieldType),
+                    Value = value,
+                    MemberInfo = fieldInfo
+                });
+            }
+
+            foreach (var propInfo in _allProperties)
+            {
+                object value = null;
+                try
+                {
+                    value = propInfo.GetValue(_objectProvider.TargetObject);
+                }
+                catch (Exception)
+                {
+                    //ignore
+                }
+
+                exposedItems.Add(new ObjectExposedItem
+                {
+                    FieldName = propInfo.Name,
+                    DisplayValue = value?.ToString(),
+                    IsPrimitive = IsPrimitive(propInfo.PropertyType),
+                    Value = value,
+                    MemberInfo = propInfo
+                });
+            }
+
+            foreach (var methodInfo in _allMethods)
+            {
+                if (methodInfo.GetParameters().Length == 0)
+                {
+                    exposedItems.Add(new ObjectExposedItem
+                    {
+                        FieldName = methodInfo.Name,
+                        DisplayValue = methodInfo.ReturnType.Name,
+                        IsPrimitive = false,
+                        Value = methodInfo.ReturnType.Name,
+                        MemberInfo = methodInfo
+                    });
+                }
+            }
+
+            if (_objectProvider.TargetObject is not Array arr) return exposedItems;
+            {
+                for (var i = 0; i < arr.Length; i++)
+                {
+                    var value = arr.GetValue(i);
+                    if (value == null) continue;
+                    exposedItems.Add(new ObjectExposedItem
+                    {
+                        FieldName = $"[{i}]",
+                        DisplayValue = value.ToString(),
+                        IsPrimitive = IsPrimitive(value.GetType()),
+                        Value = value,
+                        MemberInfo = null
+                    });
+                }
+            }
+
+            return exposedItems;
+        }
+
+        public void UpdateReflectionInfos()
+        {
+            var type = _objectProvider.TargetObject.GetType();
+
+            _allFields = type.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Instance);
+            _allProperties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Instance);
+            _allMethods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        private static bool IsPrimitive(Type type)
+        {
+            return type.IsPrimitive || type.IsEnum || type == typeof(string) || type == typeof(decimal);
+        }
+
+        public class ObjectExposedItem
+        {
+            public string FieldName;
+            public string DisplayValue;
+            public object Value;
+            public bool IsPrimitive;
+            public MemberInfo MemberInfo;
+        }
+#if UNITY_EDITOR
+
+        public class CommonRuntimeObjectExposeEditor
+        {
+            private readonly Action<ObjectExposedItem> _clickEventHandler;
+            private Vector2 _scrollPos;
+
+            public CommonRuntimeObjectExposeEditor(Action<ObjectExposedItem> clickEventHandler)
+            {
+                _clickEventHandler = clickEventHandler;
+            }
+
+            public void DrawExposedItems(IEnumerable<ObjectExposedItem> exposedItems)
+            {
+                _scrollPos = EditorGUILayout.BeginScrollView(_scrollPos, EditorStyles.helpBox, GUILayout.ExpandWidth(true));
+                EditorGUILayout.BeginVertical(GUI.skin.box);
+                foreach (var exposedItem in exposedItems)
+                {
+                    EditorGUILayout.BeginHorizontal();
+                    if (exposedItem.MemberInfo is MethodInfo methodInfo && methodInfo.GetParameters().Length == 0)
+                    {
+                        if (GUILayout.Button(new GUIContent($"()", "Invoke Method"), GUILayout.Width(25)))
+                        {
+                            _clickEventHandler?.Invoke(exposedItem);
+                        }
+                    }
+                    else
+                    {
+
+                        var isExposable = !exposedItem.IsPrimitive && exposedItem.Value != null;
+                        if (isExposable)
+                        {
+                            if (GUILayout.Button(new GUIContent($"->", "Go into"), GUILayout.Width(25)))
+                            {
+                                _clickEventHandler?.Invoke(exposedItem);
+                            }
+                        }
+                    }
+
+                    EditorGUILayout.LabelField($"{exposedItem.FieldName}");
+                    if (exposedItem.Value is Object asset)
+                    {
+                        EditorGUILayout.ObjectField(asset, typeof(Object), false);
+                    }
+                    else
+                    {
+                        EditorGUILayout.LabelField($"{exposedItem.DisplayValue}");
+                    }
+
+                    EditorGUILayout.EndHorizontal();
+                }
+
+                EditorGUILayout.EndVertical();
+                EditorGUILayout.EndScrollView();
+            }
+        }
+#endif
+    }
+}
