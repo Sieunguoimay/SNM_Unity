2a32db6b24dd321d99f40fb480ae8a027d7040d2
Sieunguoimay <vuduydu1997@gmail.com>
2026-03-30 21:21:16 +0700

    fix

--- Runtime/Code/UnityEngineUtility.cs
+++ Runtime/Code/UnityEngineUtility.cs
@@ -10,7 +10,6 @@ namespace Snm.Runtime.Unity
             var go = new GameObject
             {
                 name = string.IsNullOrEmpty(name) ? $"[{typeof(TComponent).Name}]" : name,
-                hideFlags = HideFlags.DontSave
             };
 
             if (go.TryGetComponent<TComponent>(out var c)) return c;
